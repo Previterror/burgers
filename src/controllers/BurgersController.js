9f706386e2928567ac2ca633c414740b494bc481
John Previtera <johngprevitera@gmail.com>
2024-04-08 16:12:25 -0600

    getting, creating, deleting working.

--- src/controllers/BurgersController.js
+++ src/controllers/BurgersController.js
@@ -5,7 +5,11 @@ export class BurgersController extends BaseController {
     constructor() {
         super('api/burgers')
         this.router.get('', this.getBurgers)
+        this.router.post('', this.addBurgers)
+        this.router.delete('/:burgerId', this.trashBurger)
     }
+
+
     async getBurgers(request, response, next) {
         try {
             const burgers = await burgersService.getBurgers()
@@ -15,4 +19,30 @@ export class BurgersController extends BaseController {
             console.error(error);
         }
     }
+
+
+    async addBurgers(request, response, next) {
+        try {
+            const burgerData = request.body
+            const burger = await burgersService.createBurger(burgerData)
+            response.send(burger)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async trashBurger(request, response, next) {
+        try {
+            const burgerId = request.params.burgerId
+            await burgersService.trashBurger(burgerId)
+            response.send("Burger was trashed")
+        } catch (error) {
+            next(error)
+        }
+    }
+
+
+
+
+
 }
--- src/services/BurgersService.js
+++ src/services/BurgersService.js
@@ -2,6 +2,16 @@ import { dbContext } from "../db/DbContext.js"
 
 
 class BurgersService {
+    async trashBurger(burgerId) {
+        const burgerToTrash = await dbContext.Burgers.findById(burgerId)
+        if (!burgerToTrash) throw new Error(`Sorry that burger has already been trashed ${burgerId}`)
+        await dbContext.Burgers.deleteOne({ _id: burgerId })
+    }
+
+    async createBurger(burgerData) {
+        const burger = await dbContext.Burgers.create(burgerData)
+        return burger
+    }
 
     async getBurgers() {
         const burgers = await dbContext.Burgers.find()
